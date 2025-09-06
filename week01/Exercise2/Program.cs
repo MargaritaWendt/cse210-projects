420997ad02299e301a80524dac2e81e85c76dda9
MargaritaWendt <margaritaortizreategui@gmail.com>
2025-09-06 19:06:05 +0200

    exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string scoreInput = Console.ReadLine();
+        int score = int.Parse(scoreInput);
+
+
+        if (score >= 90)
+        {
+            Console.WriteLine("Your score is A.");
+        }
+        else if (score >= 80)
+        {
+            Console.WriteLine("Your score is B.");
+        }
+        else if (score >= 70)
+        {
+            Console.WriteLine("Your score is C.");
+        }
+        else if (score >= 60)
+        {
+            Console.WriteLine("Your score is D.");
+        }
+        else
+        {
+            Console.WriteLine("Your score is F.");
+        }
+
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratution! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you missed the course.");
+        }
     }
 }
