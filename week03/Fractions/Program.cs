709c5bf7f4a255097f80e7bc9f8aa79eb3727f99
MargaritaWendt <margaritaortizreategui@gmail.com>
2025-09-17 16:40:13 +0200

    Steps until 5

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction n1 = new Fraction();
+        Console.WriteLine(n1.GetTop());
+        Console.WriteLine(n1.GetBottom());
+
+        Fraction n2 = new Fraction(6);
+        Console.WriteLine(n2.GetTop());
+        Console.WriteLine(n2.GetBottom());
+
+        Fraction n3 = new Fraction(6, 7);
+        Console.WriteLine(n3.GetTop());
+        Console.WriteLine(n3.GetBottom());
     }
 }
