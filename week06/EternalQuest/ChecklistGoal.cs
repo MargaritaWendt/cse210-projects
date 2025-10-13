df3676f1736968fbf5146e086d5c843e51dc0d80
MargaritaWendt <margaritaortizreategui@gmail.com>
2025-10-13 14:43:59 +0200

    structure Checklist Goal

--- week06/EternalQuest/ChecklistGoal.cs
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -1,4 +1,44 @@
-public class ChecklistGoal
+public class ChecklistGoal : Goal
 {
-    
+    private int _amountCompleted;  
+    private int _target;
+    private int _bonus;
+
+    public ChecklistGoal(string name, string description, int points, int target, int bonus)
+    : base(name, description, points)
+    {
+        _target = target;
+        _bonus = bonus;
+        _amountCompleted = 0;
+    }
+
+    public override void RecordEvent()
+    {
+        _amountCompleted++;
+        Console.WriteLine($"Event recorded for checklist goal: {_shortName} (+{_points} points)");
+
+        if (_amountCompleted == _target)
+        {
+            Console.WriteLine($"Congratulations! You completed {_shortName} and earned a bonus of {_bonus} points!");
+        }
+    }
+
+    public override bool IsComplete()
+    {
+        return _amountCompleted >= _target;
+    }
+
+    public override string GetDetailsString()
+    {
+        string checkbox = IsComplete() ? "[X]" : "[ ]";
+        return $"{checkbox} {_shortName} ({_description}) -- Currently completed: {_amountCompleted}/{_target}";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"ChecklistGoal:{_shortName},{_description},{_points},{_bonus},{_amountCompleted},{_target}";
+    }
+
+
+
 }
