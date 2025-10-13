33cd0330daf9e2696b78f21736ce0cd4878c8a5b
MargaritaWendt <margaritaortizreategui@gmail.com>
2025-10-13 14:02:20 +0200

    structure SimpleGoal class

--- week06/EternalQuest/SimpleGoal.cs
+++ week06/EternalQuest/SimpleGoal.cs
@@ -1,4 +1,33 @@
-public class SimpleGoal
+public class SimpleGoal : Goal
 {
-    
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, int points)
+        : base(name, description, points)
+    {
+        _isComplete = false;
+    }
+
+    public override void RecordEvent()
+    {
+        _isComplete = true;
+
+        Console.WriteLine($"Congratulations! You have earned {_points} points!");
+    }
+
+    public override bool IsComplete()
+    {
+        return _isComplete;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"SimpleGoal:{_shortName},{_description},{_points},{_isComplete}";
+    }
+
+    public override string GetDetailsString()
+    {
+        string checkbox = _isComplete ? "[X]" : "[ ]";
+        return $"{checkbox} {_shortName} ({_description})";
+    }
 }
