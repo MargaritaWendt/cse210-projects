596ad2218c053fc832b91b5854758618a60fcc65
MargaritaWendt <margaritaortizreategui@gmail.com>
2025-10-13 14:35:39 +0200

    structure Eternal Goal

--- week06/EternalQuest/EternalGoal.cs
+++ week06/EternalQuest/EternalGoal.cs
@@ -1,4 +1,22 @@
-public class EternalGoal
+public class EternalGoal : Goal
 {
-    
+    public EternalGoal(string name, string description, int points)
+         : base(name, description, points)
+    {
+    }
+
+    public override void RecordEvent()
+    {
+        Console.WriteLine($"Event recorded for eternal goal: {_shortName} (+{_points} points)");
+    }
+
+    public override bool IsComplete()
+    {
+        return false;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"EternalGoal:{_shortName},{_description},{_points}";
+    }
 }
